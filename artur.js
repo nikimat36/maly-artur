82daea8b0f97f4f5c00a692862d0c957e7f91184
nikimat36 <74063319+nikimat36@users.noreply.github.com>
2025-08-25 10:42:12 +0200

    Update artur.js

--- artur.js
+++ artur.js
@@ -17,7 +17,7 @@ puppy.age = Math.floor((new Date() - new Date(puppy.born.value)) / (1000*60*60*2
 
 
 var events = [
-	{ date: "2025-07-31", label: "Narozen", 			color: "blue", 		note: "Odhadované datum narození" },
+	{ date: puppy.born.value, label: "Narozen", 		color: "blue", 		note: "Odhadované datum narození" },
 	{ date: "2025-08-21", label: "Odchyt", 				color: "blue", 		note: "Odchycen & Převezen k Niki" },
 	{ date: "2025-08-22", label: "Odčervení", 			color: "purple", 	note: "1. odčervení (mikro-dávka)" },
 	{ date: "2025-08-24", label: "Pod Spolkem", 		color: "blue", 		note: "Převezen k Barči, ofiko pod Spolkem" },
@@ -27,4 +27,5 @@ var events = [
 	{ date: "2025-09-21", label: "Odčervení", 			color: "purple", 	note: "4. dávka odčervení" },
 	{ date: "2025-09-18", label: "Připraven k adopci", 	color: "blue", 	note: "Připraven k adopci" },
 ];
+
 
